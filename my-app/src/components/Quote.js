272570427e45869ba2d37ac4cca0c10b1e3b8845
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-12-12 13:40:58 -0800

    setup nicely

---
+++ my-app/src/components/Quote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+const Quote = props => {
+
+  return(
+    <div>
+      <h1>Kanye West Quotes</h1>
+      <button>Get Quote!</button>
+    </div>
+  )
+}
+
+export default Quote;
